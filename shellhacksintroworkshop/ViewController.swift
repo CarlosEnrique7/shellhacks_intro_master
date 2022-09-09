6d91a7b13cc3ffc58617ce3b82f9ac8a3ad0d54f
Carlos Chavez <cenric09@gmail.com>
2022-09-09 15:13:19 -0400

    initial commit

--- shellhacksintroworkshop/ViewController.swift
+++ shellhacksintroworkshop/ViewController.swift
@@ -8,12 +8,53 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var playerCard: UIImageView!
+    @IBOutlet weak var computerCard: UIImageView!
+    @IBOutlet weak var playerScoreLabel: UILabel!
+    @IBOutlet weak var computerScoreLabel: UILabel!
+    @IBOutlet weak var outcomeLabel: UILabel!
+    
+    
+    var playerScore: Int = 0
+    var computerScore: Int = 0
+    var outcome: String = ""
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func drawCard(_ sender: Any) {
+        let playerNum: Int = Int.random(in: 2...14)
+        let computerNum: Int = Int.random(in: 2...14)
+     
+        playerCard.image = UIImage(named: String(playerNum))
+        computerCard.image = UIImage(named: String(computerNum))
+        
+        // compare the two number and increase the score of the player with the largest one
+        compare(playerNum: playerNum, computerNum: computerNum)
+        
+    }
+    
+    func compare(playerNum: Int, computerNum: Int) {
+        if playerNum > computerNum {
+            outcome = "You win!"
+            outcomeLabel.textColor = UIColor.green
+            playerScore += 1
+        } else if computerNum > playerNum {
+            outcome = "You lose."
+            outcomeLabel.textColor = UIColor.red
+            computerScore += 1
+        } else {
+            outcomeLabel.textColor = UIColor.black
+            outcome = "Draw."
+        }
+        
+        playerScoreLabel.text = String(playerScore)
+        computerScoreLabel.text = String(computerScore)
+        outcomeLabel.text = outcome
+        
+    }
 }
 
